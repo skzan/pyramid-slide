1ebd1ef27a242c0d84185571c03636f7ef8d2016
Shireen Korkzan <skzan15@sbcglobal.net>
2018-01-22 20:34:13 -0500

    First commit

---
+++ pyramid-slide.js
@@ -0,0 +1,29 @@
+function drawPyramid() {
+
+    document.getElementById("pyramid").innerHTML = "";
+
+    var height = document.getElementById("slide").value;
+    var symbol = document.getElementsByName("brickType")[0].value;
+
+    for (var row = 0; row < height; row++) {
+
+        var numBricks = row + 2;
+        var numSpaces = height - row - 1;
+
+        var rowStr = "";
+
+        for (var i = 0; i < numSpaces; i++) {
+            var spaceChar = "&nbsp";
+            rowStr += spaceChar;
+        }
+        for (var i = 0; i < numBricks; i++) {
+            rowStr += "#";
+        }
+
+        rowElem = document.createElement("p");
+        rowElem.innerHTML = rowStr;
+        document.getElementById("pyramid").appendChild(rowElem);
+
+        document.getElementById("pyramidHeight").innerHTML = height;
+    }
+};
